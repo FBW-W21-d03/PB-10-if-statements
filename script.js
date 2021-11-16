0f7031b6f206d3b36e4c21d7bdecb5848d93c144
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-16 09:54:03 +0100

    Orhan's lösung

---
+++ script.js
@@ -0,0 +1,16 @@
+let isLoggedIn = false;
+
+if (isLoggedIn == false) {
+    console.log("Bitte melde dich an");
+} else {
+    console.log("Du bist erfolgreich angemeldet");
+}
+
+
+isLoggedIn = true;
+
+if (isLoggedIn == false) {
+    console.log("Bitte melde dich an");
+} else {
+    console.log("Du bist erfolgreich angemeldet");
+}
