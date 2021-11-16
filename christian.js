8c0662387d049bf3437040a70fa814321472e669
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-11-16 09:51:00 +0100

    Lösung von Christian

---
+++ christian.js
@@ -0,0 +1,8 @@
+// let isLoggedIn = false;
+let isLoggedIn = true;
+
+if (isLoggedIn == true) {
+  console.log("Du bist erfolgreich eingeloggt"); // if true
+} else {
+  console.log("Bitte melde dich an"); // if false
+}
