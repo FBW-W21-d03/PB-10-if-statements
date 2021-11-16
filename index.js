597f1e88ec3912be3a8774cc4838b120c5b74dce
H94ssein44 <alali.52@yahoo.com>
2021-11-16 10:26:20 +0100

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let isLoggedIn = true;
+ isLoggedIn = false;
+
+if (isLoggedIn) {
+    console.log ( "angemeldet" );
+}
+else {
+    console.log("abgemeldet");
+}
+
+ isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log ( " Du bist erfolgreich angemeldet " );
+}
+else {
+    console.log("Bitte melde dich an");
+}
