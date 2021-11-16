be0b291d748be4b87dde698819db27cf8dc0b7f8
slavablack <veaceslav.ciornii@magenta.de>
2021-11-16 10:04:10 +0100

    Statement Veaceslav

---
+++ statement.js
@@ -0,0 +1,16 @@
+//1
+let isLoggedIn=false;
+if (isLoggedIn) 
+{ console.log ("Du bist erfolgreich angemeldet")
+}
+else{
+    console.log("Bitte melde dich an")
+};
+//2
+let isLoggedIn2=true;
+if (isLoggedIn2) 
+{ console.log ("Du bist erfolgreich angemeldet")
+}
+else{
+    console.log("Bitte melde dich an")
+};
