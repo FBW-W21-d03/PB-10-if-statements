ce7f0af39b65cfebfe62ef8b92b456875128b0a8
Dennisbullys <d.rochus@hotmail.de>
2021-11-16 10:07:16 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,24 @@
+console.log("if-statements")
+
+// Aufgabenstellung Teil 1
+
+// 1. Erstelle eine Variable mit dem Namen isLoggedIn und fülle sie mit dem Wert false (abgemeldet)
+
+let isLoggedIn = true;
+
+// 2. Erstelle eine Fallabfrage.
+if (isLoggedIn) {
+  console.log("abgemeldet");
+}
+// 3. Falls der Nutzer abgemeldet ist, schreibe eine Konsolenausgabe mit dem Text "Bitte melde dich an".
+else {
+  console.log("Bitte melde dich an");
+}
+
+// 4. Falls der Nutzer angemeldet ist, schreibe eine Konsolenausgabe mit dem Text "Du bist erfolgreich angemeldet".
+
+if (isLoggedIn) {
+  console.log("Du bist erfolgreich angemeldet");
+} 
+
+
