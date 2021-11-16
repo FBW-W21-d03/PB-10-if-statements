6b79be2368523ae9e6c6edc93db0e7d98d47e064
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-16 10:40:39 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,9 @@
+// let isLoggedIn = false;
+ let isLoggedIn = true;
+
+
+if (isLoggedIn == true){
+    console.log("Du bist erfolgreich aingeloggt");
+} else{
+    console.log("Bitte anmelde dich an!");
+}
