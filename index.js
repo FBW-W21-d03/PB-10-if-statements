76cbd3008dfabe384a0b2eedd240e30518915d1d
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-16 10:11:20 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,17 @@
+let isLoggedIn = false;
+if (isLoggedIn) {
+    console.log("Du bist erfolgreich angemeldet");
+} else {
+     console.log("Bitte melde dich an");
+}
+
+isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log("Du bist erfolgreich angemeldet");
+} else {
+    console.log("Bitte melde dich an");
+}
+
+
+
