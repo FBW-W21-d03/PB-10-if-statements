e231cfa789b109686a893920e74a1ec0798e44a2
sprotte <metatron1972@live.de>
2021-11-16 09:52:08 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,22 @@
+myFalse = new Boolean(false);   // initialer Wert von false
+g = Boolean(myFalse);       // initialer Wert von  true
+myString = new String('guten tag'); // string objekt
+s = Boolean(myString);      //  initialer Wert von true
+
+//Boolean Objektes mit dem Anfangswert false
+
+let bNoParam = new Boolean();
+let bZero = new Boolean(0);
+let bNull = new Boolean(null);
+let bEmptyString = new Boolean('');
+let bfalse = new Boolean(false);
+
+
+//Boolean Objektes mit dem Anfangswert true
+
+let btrue = new Boolean(true);
+let btrueString = new Boolean('true');
+let bfalseString = new Boolean('false');
+let bSuLin = new Boolean('Su Lin');
+let bArrayProto = new Boolean([]);
+let bObjProto = new Boolean({});
